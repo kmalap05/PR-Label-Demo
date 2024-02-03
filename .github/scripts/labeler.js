381fc07a45c61dbfac932295e9ce9c846397c319
Kedar Malap <kedarmalap456@gmail.com>
2024-02-03 12:13:22 +0530

    changes

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -1,21 +1,12 @@
 const { context, getOctokit } = require("@actions/github");
 
 async function applyLabels(octokit, owner, repo, pull_number, labels) {
-  for (const label of labels) {
-    await octokit.rest.issues.createLabel({
-      owner,
-      repo,
-      name: label.name,
-      color: label.color,
-    });
-
-    await octokit.rest.issues.addLabels({
-      owner,
-      repo,
-      issue_number: pull_number,
-      labels: [label.name],
-    });
-  }
+  await octokit.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: pull_number,
+    labels,
+  });
 }
 
 async function main() {
@@ -41,20 +32,14 @@ async function main() {
 
   for (const file of changedFiles.data) {
     if (file.filename.endsWith(".js")) {
-      // Add the label with color information
-      labelsToApply.push({
-        name: "javascript-file",
-        color: "ffcc00", // Replace with the desired color code
-      });
+      labelsToApply.push("javascript-file");
     }
     // Add more conditions as needed
   }
 
   if (labelsToApply.length > 0) {
     await applyLabels(octokit, owner, repo, pull_number, labelsToApply);
-    console.log(
-      `Labels applied: ${labelsToApply.map((label) => label.name).join(", ")}`
-    );
+    console.log(`Labels applied: ${labelsToApply.join(", ")}`);
   } else {
     console.log("No labels to apply.");
   }
